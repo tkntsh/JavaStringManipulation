a6db6c7f832f8c0072c1585e6a65511656c0d260
Ntokozo <ntokntshangase@gmail.com>
2024-12-03 17:37:02 +0200

    Java file using replace() method

---
+++ stringMethod5.java
@@ -0,0 +1,8 @@
+public class stringMethod5
+{
+    public static void main(String[] args)
+    {
+        String sentence = "Today is Monday";
+        System.out.println(sentence + "\nMonday replaced with Tuesday: " + sentence.replace("Monday", "Tuesday"));
+    }
+}
