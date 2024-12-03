7eca03b3a3025c84f6225c5933c1c870bf09306d
Ntokozo <ntokntshangase@gmail.com>
2024-12-03 17:43:05 +0200

    Java file to print characters from string

---
+++ stringMethod6.java
@@ -0,0 +1,12 @@
+public class stringMethod6
+{
+    public static void main(String[] args)
+    {
+        String sentence = "Don't Worry. Be happy!";
+
+        for(int i = 0; i<sentence.length(); i++)
+        {
+            System.out.println(sentence.charAt(i) + "\n");
+        }
+    }
+}
