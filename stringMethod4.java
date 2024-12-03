dde742b7ba8401b0335bccfc9d921e4799773a15
Ntokozo <ntokntshangase@gmail.com>
2024-12-03 17:31:39 +0200

    Java file to convert string to upper/lowercase

---
+++ stringMethod4.java
@@ -0,0 +1,9 @@
+public class stringMethod4
+{
+    public static void main(String[] args)
+    {
+        String sentence = "She said 'yes'!";
+        System.out.println("Sentence converted to uppercase: " + sentence.toUpperCase());
+        System.out.println("Sentence converted to lowercase: " + sentence.toLowerCase());
+    }
+}
