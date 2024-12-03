4ae8068bb923c5416ddc9c8f2747674907a910ef
Ntokozo <ntokntshangase@gmail.com>
2024-12-03 17:06:52 +0200

    Java file to print the index of a string

---
+++ stringMethods.java
@@ -0,0 +1,10 @@
+public class stringMethods
+{
+    public static void main(String[] args)
+    {
+        String sentence = "Please find this word. Be happy you find it!";
+        int wordFind = sentence.indexOf("find");
+
+        System.out.println("The word find appears in index: " + wordFind);
+    }
+}
